50653568e3b5fb2e3608e57045ce81d48b3c1a61
TechSavvy Group <techsavvy1013@outlook.com>
2021-07-12 22:43:44 +0900

    Changed to use getbranchpretty

--- web/js/settings.js
+++ web/js/settings.js
@@ -130,7 +130,125 @@ async function createSettingWidgets(table, settingPath, entries, branchData) {
                 sliderValue.className = "col-1";
                 sliderValue.style.textAlign = "center";
                 sliderValue.style.paddingTop = "6px";
+                input.style.paddingLeft = "0px";
+                input.style.paddingRight = "0px";
                 sliderValue.innerHTML = referenceLeaves[0].value;
+                row.append(label, input, sliderValue, browsePath, btn);
+            } else {
+                row.append(label, input, browsePath, btn);
+            }
+            table.append(row);
+        }
+    }
+}
+
+function createSettingWidgetsSync(table, settingPath, entries, branchDataArray) {
+    for (var i in entries) {
+        // var i = 0; {
+        var entry = entries[i];
+        var branchData = branchDataArray[entry.name]['.properties'];
+        var branchId = branchData.leavesIds[0];
+        var referenceLeaves = branchData.leavesProperties[branchId];
+        //make a row
+        var row = document.createElement("div");
+        row.className = "form-group row mb-2";
+        row.setAttribute("data-toggle", "tooltip");
+        row.setAttribute("data-placement", "bottom");
+        row.setAttribute("title", "There was some errors loading the data...");
+
+        // label div
+        var label = document.createElement("label");
+        label.className = "col-2";
+        label.innerHTML = entry.name;
+
+        if (referenceLeaves != undefined && referenceLeaves != null && referenceLeaves != "none") {
+            // input div
+            var input;
+            var jsonValidation = referenceLeaves.jsonValidation;
+            // change input field according to the jsonValidation schema
+            if (jsonValidation.enum != undefined) {
+                var input = document.createElement("SELECT");
+
+                var inner = "";
+                var possibleValues = jsonValidation.enum;
+                for (var i in possibleValues) {
+                    inner = inner + "<option>" + possibleValues[i] + "</option>";
+                }
+                input.innerHTML = inner;
+                input.value = referenceLeaves.value;
+                input.setAttribute("data-type", jsonValidation.type);
+                input.setAttribute("path", entry.name);
+            } else if (jsonValidation == undefined || jsonValidation.type == undefined) {
+                input = document.createElement("input");
+
+                input.value = referenceLeaves.value;
+                input.setAttribute("data-type", "string");
+                input.setAttribute("path", entry.name);
+            } else if (jsonValidation.type == "integer") {
+                input = document.createElement("input");
+
+                input.value = referenceLeaves.value;
+                input.setAttribute("type", "range");
+
+                input.setAttribute("min", jsonValidation.minimum);
+                input.setAttribute("max", jsonValidation.maximum);
+                input.setAttribute("data-type", jsonValidation.type);
+                input.setAttribute("path", entry.name);
+            } else if (jsonValidation.type == "boolean") {
+                input = document.createElement("input");
+
+                input.setAttribute("type", "checkbox");
+                if (referenceLeaves.value == 'true' || referenceLeaves.value == true)
+                    input.setAttribute("checked", true);
+                input.setAttribute("data-type", jsonValidation.type);
+                input.setAttribute("path", entry.name);
+            } else {
+                input = document.createElement("input");
+
+                input.value = referenceLeaves.value;
+                input.setAttribute("data-type", "string");
+                input.setAttribute("path", entry.name);
+            }
+
+            if (jsonValidation.type == "integer") {
+                input.className = "form-control col-6 slider";
+            } else if (jsonValidation.type == "boolean") {
+                input.className = "col-7 form-check-input";
+                input.style.position = "relative";
+                input.style.marginLeft = "0px";
+            } else {
+                input.className = "form-control col-7";
+            }
+
+            // add browse Path 
+            var browsePath = document.createElement("input");
+            browsePath.className = "form-control col-7 hidden";
+
+            var browsePathValue = "";
+            for (var i = 0; i < referenceLeaves.length; i++)
+                browsePathValue += (i == 0 ? "" : ",") + referenceLeaves[i].browsePath;
+            browsePath.value = browsePathValue;
+            browsePath.setAttribute("type", "hidden");
+
+            // apply button
+            var btn = document.createElement("BUTTON");   // Create a <button> element
+            btn.className = "btn btn-primary btn-sm col-1 ml-2";
+            btn.id = "apply-" + entry.id;
+            btn.innerHTML = 'Apply';
+            btn.onclick = saveSettingValue;
+
+            if (jsonValidation.description == undefined)
+                row.setAttribute("title", "Please add description in the json validaiton schema");
+            else
+                row.setAttribute("title", jsonValidation.description);
+            if (jsonValidation.type == "integer") {
+                var sliderValue = document.createElement("div");
+                sliderValue.className = "col-1";
+                sliderValue.style.textAlign = "center";
+                sliderValue.style.paddingTop = "6px";
+                input.style.paddingLeft = "0px";
+                input.style.paddingRight = "0px";
+                sliderValue.innerHTML = referenceLeaves.value;
                 row.append(label, input, sliderValue, browsePath, btn);
             } else {
                 row.append(label, input, browsePath, btn);
@@ -192,7 +310,7 @@ function initialize_settings() {
                 else {
                     //we have at least one entry
                     table.empty();
-                    createSettingWidgets(table, settingPath, msgs[0].children, branchData);
+                    createSettingWidgetsSync(table, settingPath, msgs[0].children, branchData);
                 }
             }
         });
